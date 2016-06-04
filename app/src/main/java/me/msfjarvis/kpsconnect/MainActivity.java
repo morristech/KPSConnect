4d4c04fe3249815a19b5f6d8afbd9358ff92745c
MSF Jarvis <msfjarvis@gmail.com>
2016-06-04 17:50:39 +0530

    [SQUASHED] Add connection checks, intro dialog
    Signed-off-by: MSF Jarvis <msfjarvis@gmail.com>

--- app/src/main/java/me/msfjarvis/kpsconnect/MainActivity.java
+++ app/src/main/java/me/msfjarvis/kpsconnect/MainActivity.java
@@ -1,17 +1,29 @@
 package me.msfjarvis.kpsconnect;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 import me.msfjarvis.kpsconnect.BlogFragment;
 
@@ -19,11 +31,62 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
     public NavigationView navigationView;
+    private static final String TAG = "KPS Connect";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Context context = getApplicationContext();
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        final boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        Log.i(TAG, "Connection status received");
+        if (isConnected) {
+            try {
+                Log.i(TAG, "Connection is a go!");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                finish();
+            }
+        } else {
+            Log.i(TAG, "No bloody connection");
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Log.i(TAG, "Setting error text");
+            Toast.makeText(context, "No connection. The app needs an active internet connection to function",Toast.LENGTH_LONG).show();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                Log.i(TAG, "Killing activity");
+                finish();
+
+            }
+        }
+        SharedPreferences pref =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        String is_first_run = pref.getString("is_first_run","n/a");
+        if (is_first_run.equals("n/a")) {
+            new MaterialDialog.Builder(this)
+                    .title(R.string.app_name)
+                    .customView(R.layout.intro, false)
+                    .positiveText("OK")
+                    .show();
+            SharedPreferences.Editor edit = pref.edit();
+            edit.putString("is_first_run", "no");
+            edit.commit();
+        }
+
+
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.inflateMenu(R.menu.menu_main);
@@ -100,6 +163,7 @@ public class MainActivity extends AppCompatActivity {
         onHome();
 
     }
+
 }
 
 
