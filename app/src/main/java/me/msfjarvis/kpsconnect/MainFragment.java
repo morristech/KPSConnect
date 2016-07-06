d296a1741e1af3a2bb4e61ebd9168406d9802d60
MSF Jarvis <msfjarvis@gmail.com>
2016-07-06 15:36:42 +0530

    disable proguard, add dynamic homepage
    Signed-off-by: MSF Jarvis <msfjarvis@gmail.com>

--- app/src/main/java/me/msfjarvis/kpsconnect/MainFragment.java
+++ app/src/main/java/me/msfjarvis/kpsconnect/MainFragment.java
@@ -4,19 +4,30 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.ViewGroup;;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.squareup.picasso.Picasso;
+//import com.squareup.picasso.Picasso;
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import okhttp3.Request;
 
 public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, parent, false);
+    }
+    public class stringLoader {
+        OkHttpClient client = new OkHttpClient();
+
+        String run(String url) throws IOException {
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        }
     }
 
     @Override
@@ -24,9 +35,16 @@ public class MainFragment extends Fragment {
         ImageView homeImage = (ImageView) view.findViewById(R.id.homeImageView);
         TextView homeText = (TextView) view.findViewById(R.id.homeTextView);
         String imageUri = "https://github.com/MSF-Jarvis.png";
-        Picasso
+        /*Picasso
                 .with(getContext())
                 .load(imageUri)
-                .into(homeImage);
+                .into(homeImage);*/
+        stringLoader newsString = new stringLoader();
+        try{
+            String response = newsString.run("https://raw.githubusercontent.com/MSF-Jarvis/local_manifests/cm-13.0/local_manifest.xml");
+            homeText.setText(response);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
