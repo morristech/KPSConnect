11e5ed5700efa73af97674df2ecf2121624ecec1
Harsh Shandilya <msfjarvis@gmail.com>
2016-07-16 14:24:36 +0530

    Improve share intent, clean code
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/src/main/java/me/msfjarvis/kpsconnect/BlogFragment.java
+++ app/src/main/java/me/msfjarvis/kpsconnect/BlogFragment.java
@@ -1,7 +1,7 @@
 package me.msfjarvis.kpsconnect;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,18 +11,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 public class BlogFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_blog, parent, false);
     }
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         final WebView blogWebView = (WebView) view.findViewById(R.id.blogwebview);
         blogWebView.loadUrl("http://www.khaitanpublicschool.com/blog");
-        blogWebView.setWebViewClient(new WebViewClient());
         WebSettings webSettings = blogWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setLoadsImagesAutomatically(true);
@@ -33,9 +32,11 @@ public class BlogFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 String shareUrl = blogWebView.getUrl();
+                String shareTitle = blogWebView.getTitle();
                 Intent shareIntent = new Intent(Intent.ACTION_SEND);
                 shareIntent.setType("text/plain");
-                shareIntent.putExtra(Intent.EXTRA_TEXT, shareUrl);
+                String finalShareString = shareTitle + " - " + shareUrl;
+                shareIntent.putExtra(Intent.EXTRA_TEXT, finalShareString);
                 try{
                     startActivity(Intent.createChooser(shareIntent, "Share link using"));
                 }catch (Exception e){
@@ -44,5 +45,4 @@ public class BlogFragment extends Fragment {
             }
         });
     }
-
 }
