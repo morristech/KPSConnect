b211c7cbd62fa9cc4e576e6fa8999e7480208fe6
Harsh Shandilya <msfjarvis@gmail.com>
2016-08-12 01:49:14 +0530

    v2.5.0-beta : We can now parse posts as well
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/src/main/java/me/msfjarvis/kpsconnect/MainFragment.java
+++ app/src/main/java/me/msfjarvis/kpsconnect/MainFragment.java
@@ -1,14 +1,29 @@
 package me.msfjarvis.kpsconnect;
 
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import com.einmalfel.earl.EarlParser;
+import com.einmalfel.earl.Feed;
+import com.einmalfel.earl.Item;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class MainFragment extends Fragment {
+    public String link = "http://khaitanpublicschool.com/blog/feed";
+    public String TAG = "PostsParser";
+    public Map posts = new HashMap();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
@@ -16,14 +31,50 @@ public class MainFragment extends Fragment {
     }
 
     public void onViewCreated(final View view, Bundle savedInstanceState) {
-        Button button = (Button) view.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent introIntent = new Intent("me.msfjarvis.kpsconnect.MAININTROACTIVITY");
-                startActivity(introIntent);
-            }
-        });
+        new PullInPosts().execute();
 
     }
-}
+
+    private class PullInPosts extends AsyncTask<Void, Void, String> {
+        @Override
+        public String doInBackground(Void... Void) {
+            try {
+                InputStream inputStream = new URL(link).openConnection().getInputStream();
+                Feed feed = EarlParser.parseOrThrow(inputStream, 0);
+                Log.i(TAG, "Processing feed: " + feed.getTitle());
+                for (Item item : feed.getItems()) {
+                    String title = item.getTitle();
+                    String link = item.getLink();
+                    posts.put(title,link);
+
+                    Log.i(TAG, "Item title: " + (title == null ? "N/A" : title));
+                }
+            } catch (MalformedURLException exc) {
+                exc.printStackTrace();
+            } catch (org.xmlpull.v1.XmlPullParserException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (java.util.zip.DataFormatException e) {
+                e.printStackTrace();
+            }
+            return "void";
+        }
+        @Override
+        protected void onPostExecute(String result)
+        {
+            Set set = posts.entrySet();
+            Iterator iterator = set.iterator();
+            while (iterator.hasNext()) {
+                Map.Entry m = (Map.Entry) iterator.next();
+                String key = (String) m.getKey();
+                Log.i("POST","title:"+key+" title:"+posts.get(key));
+
+            }
+
+
+            }
+
+        }
+    }
+
