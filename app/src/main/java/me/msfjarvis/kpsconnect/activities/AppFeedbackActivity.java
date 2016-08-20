7936aab37f61fc42a233bbf5ca5595e71a43384d
Harsh Shandilya <MSF-Jarvis@users.noreply.github.com>
2016-08-20 12:43:44 +0530

    version 3.0.0-beta, add buildscript
    Cheers to @xdevs23 for his shit build script that this commit adds, also bring in a new feedback mechanism to kill the email app dependency
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/src/main/java/me/msfjarvis/kpsconnect/activities/AppFeedbackActivity.java
+++ app/src/main/java/me/msfjarvis/kpsconnect/activities/AppFeedbackActivity.java
@@ -1,19 +1,22 @@
 package me.msfjarvis.kpsconnect.activities;
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
-
+import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import me.msfjarvis.kpsconnect.R;
+import me.msfjarvis.kpsconnect.utils.APIThread;
 
 
 public class AppFeedbackActivity extends AppCompatActivity {
+
+    String emailText,subjectText,messageText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,41 +31,40 @@ public class AppFeedbackActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View v) {
+        EditText email = (EditText) findViewById(R.id.email);
         EditText subject = (EditText) findViewById(R.id.subject);
         EditText message = (EditText) findViewById(R.id.message);
-        String subjectText = subject.getText().toString();
-        String messageText = message.getText().toString();
-        if (subjectText.equals("")) {
-            Snackbar.make(v, "No subject line!", Snackbar.LENGTH_SHORT).show();
-        } else if (messageText.equals("")) {
-            Snackbar.make(v, "No message body!", Snackbar.LENGTH_SHORT).show();
-        } else {
-            Intent sendIntent = new Intent(Intent.ACTION_VIEW);
-            sendIntent.setType("plain/text");
-            sendIntent.setData(Uri.parse("app@khaitanpublicschool.com"));
-            sendIntent.setClassName("com.google.android.gm", "com.google.android.gm.ComposeActivityGmail");
-            sendIntent.putExtra(Intent.EXTRA_EMAIL, R.string.email);
-            sendIntent.putExtra(Intent.EXTRA_SUBJECT, subjectText);
-            sendIntent.putExtra(Intent.EXTRA_TEXT, messageText);
-            try {
-                startActivity(sendIntent);
-                finish();
-            }catch (ActivityNotFoundException e){
-                Intent sendMessageIntent = new Intent(Intent.ACTION_SEND);
-                sendMessageIntent.setType("text/plain");
-                sendMessageIntent.putExtra(Intent.EXTRA_EMAIL, R.string.email);
-                sendMessageIntent.putExtra(Intent.EXTRA_SUBJECT, subjectText);
-                sendMessageIntent.putExtra(Intent.EXTRA_TEXT, messageText);
-                if (sendMessageIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(sendMessageIntent);
-                    finish();
-                }else{
-                    Snackbar.make(v,"No email app found! Can't send feedback!",Snackbar.LENGTH_LONG).show();
-                }
-            }catch (Exception e){
-                Snackbar.make(v,"Tough luck mate, email couldn't be sent :(",Snackbar.LENGTH_LONG).show();
-            }
-        }
+        emailText = email.getText().toString();
+        subjectText = subject.getText().toString();
+        messageText = message.getText().toString();
+        new sendFeedback().execute();
+    }
+    private class sendFeedback extends AsyncTask<Void,Void,String> {
 
+        @Override
+        protected String doInBackground(Void... params){
+            if(emailText!="" || subjectText!="" || messageText!="") {
+                JSONObject feedback = new JSONObject();
+                try {
+                    feedback.put("email",emailText);
+                    feedback.put("subject",subjectText);
+                    feedback.put("message",messageText);
+                } catch (JSONException exc) {
+                    Toast.makeText(getApplicationContext(), exc.toString(), Toast.LENGTH_SHORT).show();
+                }
+                new APIThread().sendFeedback(feedback);
+            }else{
+                if (emailText.equals("")){
+                    Toast.makeText(getApplicationContext(), "Enter email!", Toast.LENGTH_SHORT).show();
+                }
+                else if (subjectText.equals("")){
+                    Toast.makeText(getApplicationContext(), "Enter subject!", Toast.LENGTH_SHORT).show();
+                }
+                else if (messageText.equals("")){
+                    Toast.makeText(getApplicationContext(), "Enter message!", Toast.LENGTH_SHORT).show();
+                }
+            }
+            return "execution complete";
+        }
     }
 }
--- app/src/main/java/me/msfjarvis/kpsconnect/utils/APIThread.java
+++ app/src/main/java/me/msfjarvis/kpsconnect/utils/APIThread.java
@@ -1,25 +1,35 @@
 package me.msfjarvis.kpsconnect.utils;
 
 import android.util.Log;
-
 import com.afollestad.bridge.Bridge;
 import com.afollestad.bridge.BridgeException;
-import com.afollestad.bridge.Request;
-
 import org.json.JSONObject;
 
 public class APIThread extends Thread {
     public void run(String BASE_URL, JSONObject postContent){
         try{
-            Request request = Bridge
+            Bridge
                     .post(BASE_URL)
                     .body(postContent)
                     .header("Content-Type","application/json")
+                    .header("OAuth-Token","test-keys")
                     .request();
         }catch (BridgeException exc){
             Log.d("Bridge",exc.toString());
         }
 
 
+    }
+    public void sendFeedback(JSONObject feedback){
+        try{
+            Bridge
+                    .post("https://api.msfjarvis.me/content/feedback")
+                    .header("Content-Type","application/json")
+                    .header("OAuth-Token","test-keys")
+                    .body(feedback)
+                    .request();
+        }catch (BridgeException exc){
+            Log.d("Bridge",exc.toString());
+        }
     }
 }
