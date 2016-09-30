203206640a14c1bfd6b6c858a5483c2b1a3103f4
Harsh Shandilya <msfjarvis@gmail.com>
2016-09-30 12:24:03 +0530

    Use a much better connection check
    And very minor cleanup as well
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/src/main/java/me/msfjarvis/kpsconnect/MainActivity.java
+++ app/src/main/java/me/msfjarvis/kpsconnect/MainActivity.java
@@ -1,11 +1,8 @@
 package me.msfjarvis.kpsconnect;
 
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -31,6 +28,7 @@ import com.mikepenz.aboutlibraries.ui.LibsSupportFragment;
 
 import org.xdevs23.ui.utils.BarColors;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -67,13 +65,7 @@ public class MainActivity extends AppCompatActivity implements OnRssLoadListener
         setContentView(R.layout.activity_main);
         BarColors.setStatusBarColor(R.color.colorPrimaryDark,getWindow());
         BarColors.setNavigationBarColor(R.color.colorPrimaryDark,getWindow());
-        final Context context = this;
-        ConnectivityManager cm =
-                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        final boolean isConnected = activeNetwork != null &&
-                activeNetwork.isConnectedOrConnecting();
-        if (isConnected) {
+        if (isConnected()) {
             Log.d("KPSConnect", "Internet is connected!");
             pref = PreferenceManager.getDefaultSharedPreferences(this);
             edit = pref.edit();
@@ -105,6 +97,17 @@ public class MainActivity extends AppCompatActivity implements OnRssLoadListener
         }
     }
 
+    public boolean isConnected() {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
     public void onHome() {
         if(isPaused) return;
