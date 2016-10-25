71cd9329ac827145399ee454cdbcc83ed91e190a
Harsh Shandilya <msfjarvis@gmail.com>
2016-10-25 09:56:16 +0530

    Nuke unused slide from IntroActivity
    Minor tweaks to script
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- upload_artifact.sh
+++ upload_artifact.sh
@@ -6,10 +6,10 @@ SIGNATURE="$(printf "${TIMESTAMP}${TOKEN}" \
              | openssl dgst -sha256 -hmac "${SECRET}" -binary \
              | openssl enc -base64)"
 
-[ -f app/app-release.apk] && mv app/app-release.apk app/kpsconnect-beta-$(git -C ${pwd} rev-parse --short=7 HEAD).apk || exit 1
+[ -f app/app-release.apk] && mv app/app-release.apk app/kpsconnect-beta-$(git rev-parse --short=7 HEAD).apk || exit 1
 # order does not matter; any skipped fields in Authorization will be set to defaults
 curl -T \
 	--header "Timestamp: ${TIMESTAMP}" \
 	--header "Token: ${TOKEN}" \
 	--header "Authorization: Signature keyId='${KEYID}',signature='${SIGNATURE}'" \
-	"app/kpsconnect-beta-$(git -C ${pwd} rev-parse --short=7 HEAD).apk" "https://kpsconnect.msfjarvis.me/ci-builds"
+	"app/kpsconnect-beta-$(git rev-parse --short=7 HEAD).apk" "https://kpsconnect.msfjarvis.me/ci-builds"
