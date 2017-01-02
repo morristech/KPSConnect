b350a1d83dab7267c952b674432c502852d85699
Ritayan Chakraborty <ritayanout@gmail.com>
2017-01-02 14:51:56 +0530

    KPSConnectBuilder : Send APKs with the notification

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 bash buildReleaseApp.sh
-if [ $? -eq 0 ];then
-export tg_message="\[KPSConnect] Build number $BUILD_NUMBER succeeded, changes [here](http://jenkins.msfjarvis.me/job/afh-browser-master/$BUILD_NUMBER/changes). Build artifact [here](http://jenkins.msfjarvis.me/job/afh-browser-master/lastSuccessfulBuild/artifact/app/app-nightly-release.apk)"
+if [ -f app/app-release.apk ];then
+export tg_message="\[KPSConnect] Build number $BUILD_NUMBER succeeded, changes [here](http://jenkins.msfjarvis.me/job/afh-browser-master/$BUILD_NUMBER/changes). Build a$
 curl "https://api.telegram.org/bot$TG_BOT_ID/sendmessage" --data "text=$tg_message&chat_id=$TG_GRP_ID&parse_mode=Markdown" 2>&1 >/dev/null
-curl "https://api.telegram.org/bot$TG_BOT_ID/sendmessage" --data "text=$tg_message&chat_id=$TG_GRP_ID_2&parse_mode=Markdown" 2>&1 >/dev/null
+apk="app-release.apk"
+curl -F chat_id="$TG_GRP_ID" -F document=@$apk "https://api.telegram.org/bot$TG_BOT_ID/sendDocument"
 else
 export tg_message="\[KPSConnect] Build number $BUILD_NUMBER failed, changes [here](http://jenkins.msfjarvis.me/job/afh-browser-master/$BUILD_NUMBER/changes). Console output [here](http://jenkins.msfjarvis.me/job/afh-browser-master/lastBuild/console)"
 curl "https://api.telegram.org/bot$TG_BOT_ID/sendmessage" --data "text=$tg_message&chat_id=$TG_GRP_ID&parse_mode=Markdown" 2>&1 >/dev/null
-curl "https://api.telegram.org/bot$TG_BOT_ID/sendmessage" --data "text=$tg_message&chat_id=$TG_GRP_ID_2&parse_mode=Markdown" 2>&1 >/dev/null
 fi
